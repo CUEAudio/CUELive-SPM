f8d553c98ac01b56dc367ca8660d5fdf31f2c9f1
support <support@cueaudio.com>
2024-10-28 18:57:55 +0000

    Update CUELive to v0.3.9.12-dynfmwk-trial5

--- Package.swift
+++ Package.swift
@@ -32,8 +32,8 @@ let package = Package(
         ),
 		.binaryTarget(
 			name: "CUELive",
-			url: "https://s3.amazonaws.com/swift-package-manager/binaries/CUELive/3.9.12/CUELive.3.9.12.xcframework.zip",
-			checksum: "cbdf76522ad9d971a2d7bbbea28be989351ed679ab888f0a7f77a3785289287d"
+			url: "https://s3.amazonaws.com/swift-package-manager/binaries/CUELive/0.3.9.12-dynfmwk-trial5/CUELive.0.3.9.12-dynfmwk-trial5.xcframework.zip",
+			checksum: "d270aa130bceb3841c4824868baa1a0d37eee653d48206f1e4f035030519cf85"
 		),
         .binaryTarget(
              name: "CueLightShow",
